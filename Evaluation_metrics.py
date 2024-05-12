9a3cc9ec2015e46d2d1be83253cdd33e868183f3
Baljyot Singh Modi <baljyot22133@iiitd.ac.in>
2024-05-13 02:18:10 +0530

    Univariate LSTM final done

--- Evaluation_metrics.py
+++ Evaluation_metrics.py
@@ -1,6 +1,6 @@
 import numpy as np
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score,precision_score, recall_score, f1_score
-
+import pandas as pd
 def MAPE(y_true, y_pred): 
     y_true, y_pred = np.array(y_true), np.array(y_pred)
     return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
@@ -33,3 +33,32 @@ def recall(y_true, y_pred):
 
 def f1(y_true, y_pred):
     return f1_score(y_true, y_pred)
+
+
+
+def evaluate_metrics(y_true, y_pred):
+    results = {
+        'Metric': ['MAPE', 'MAE', 'MSE', 'RMSE', 'R2', 'SMAPE'],
+        'Value': [MAPE(y_true, y_pred), 
+                  MAE(y_true, y_pred), 
+                  MSE(y_true, y_pred), 
+                  RMSE(y_true, y_pred), 
+                  R2(y_true, y_pred), 
+                  SMAPE(y_true, y_pred)]
+    }
+    df = pd.DataFrame(results)
+    
+    # Add dotted lines
+    dotted_style = [dict(selector="th", props=[("border-bottom", "1px dotted #aaaaaa")]),
+                    dict(selector="td", props=[("border-bottom", "1px dotted #aaaaaa")])]
+    
+    # Apply styling
+    styled_df = (df.style
+                 .set_properties(**{'text-align': 'center'})
+                 .format({'Value': '{:.2f}'})  # Round values to 2 decimal places
+                 .set_table_styles(dotted_style)
+                 .set_caption('Evaluation Metrics')
+                 .set_table_attributes('style="border-collapse: collapse; border: none;"')
+                 .set_properties(subset=['Metric'], **{'font-weight': 'bold',}))  # Bold and blue headers
+    
+    return styled_df
